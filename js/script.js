7feeb6046eb87545eeaa26051e2c6d156c9032c2
Koech-Eugene <eugenekoech9@gmail.com>
2024-01-17 08:39:21 +0300

    apis

--- js/script.js
+++ js/script.js
@@ -8,3 +8,12 @@ const search = document.getElementById("search")
 const newsHeader = document.getElementById("newsHeader")
 const newsInfo = document.getElementById("newsInfo")
 const searchNews = document.getElementById("newsSearch")
+
+// Get the apis 
+const api_Key = "eddebd9279ad454c8f0975735f3909b4"
+const generalNews = "https://newsapi.org/v2/top-headlines/sources?apiKey="
+const businessNews ="https://newsapi.org/v2/top-headlines/sources?category=business&apiKey="
+const entertainentNews ="https://newsapi.org/v2/top-headlines/sources?category=entertainment&apiKey="
+const techNews = "https://newsapi.org/v2/top-headlines/sources?category=technology&apiKey="
+const sportsNews = "https://newsapi.org/v2/top-headlines/sources?category=spors&apiKey="
+const Search_news = "https://newsapi.org/v2/everything?q="
